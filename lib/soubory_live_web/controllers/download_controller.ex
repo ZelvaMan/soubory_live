c7de2a1133e2740e213b8ec2a6bef64f033144f3
ZelvaMan <zelvaman16@gmail.com>
2021-08-31 17:00:33 +0200

    added: download controler checks if path is inside zip folder

--- lib/soubory_live_web/controllers/download_controller.ex
+++ lib/soubory_live_web/controllers/download_controller.ex
@@ -4,8 +4,9 @@ defmodule SouboryLiveWeb.DownloadController do
   def zip(conn, params) do
     path = params["path"]
 
-    if path == nil do
-      {:error, :path_not_valid}
+    if path == nil || !File.exists?(path) ||
+         !String.contains?(path, SouboryLive.FileHelper.zip_folder_path()) do
+      send_resp(conn, 404, " ERROR:\n file does not exist or you cant have it :D")
     else
       send_download(conn, {:file, path})
     end
